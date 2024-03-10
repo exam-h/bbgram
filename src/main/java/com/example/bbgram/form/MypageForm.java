f7f43e1202fb1e144ef1e8380e47eeb840cc4cec
Taro <taro@example.com>
2024-03-10 19:57:25 +0900

    mypageの作成(マイページアイコンと画像設定のボタンの配置)

---
+++ src/main/java/com/example/bbgram/form/MypageForm.java
@@ -0,0 +1,33 @@
+package com.example.bbgram.form;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.bbgram.validation.constraints.ImageByte;
+import com.example.bbgram.validation.constraints.ImageNotEmpty;
+
+import lombok.Data;
+
+@Data
+public class MypageForm {
+
+    private Long id;
+
+    private Long userId;
+
+    @ImageNotEmpty
+    @ImageByte(max = 2000000)
+    private MultipartFile image;
+
+    private String imageData;
+
+    private String path;
+
+    @NotEmpty
+    @Size(max = 1000)
+    private String description;
+
+    private UserForm user;
+
+}
